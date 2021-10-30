ab4a6772fda015aea34be7b1e5aff01988661a28
Leonel <leonel55sousa@gmail.com>
2021-10-30 13:46:15 -0300

    feat: add exception handling

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -26,12 +26,12 @@ public class CategoryResource {
 
 		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
-		
+
 		CategoryDTO dto = service.findById(id);
-		
+
 		return ResponseEntity.ok().body(dto);
 	}
 }
