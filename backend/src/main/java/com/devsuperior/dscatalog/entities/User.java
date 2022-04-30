e67aa823656ea20c6e0e6415babf7999db19eb42
Lonel de Sousa <leonel55sousa@gmail.com>
2022-04-30 20:31:11 -0300

    feat: organized the crud to user

--- backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
@@ -6,6 +6,7 @@ import java.util.Objects;
 import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -27,7 +28,7 @@ public class User implements Serializable {
 	private String email;
 	private String password;
 
-	@ManyToMany
+	@ManyToMany(fetch = FetchType.EAGER)//Forçar o carregamento também dos roles sempre que buscar os Users
 	@JoinTable(
 			name = "tb_user_role", 
 			joinColumns = @JoinColumn(name = "user_id"),
