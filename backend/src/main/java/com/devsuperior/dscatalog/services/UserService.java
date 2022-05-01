160036bf6839eb130e9eb50784f23b6f3c7d2131
Lonel de Sousa <leonel55sousa@gmail.com>
2022-05-01 10:20:32 -0300

    feat: crud user 100%

--- backend/src/main/java/com/devsuperior/dscatalog/services/UserService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/UserService.java
@@ -9,11 +9,13 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.dto.RoleDTO;
 import com.devsuperior.dscatalog.dto.UserDTO;
+import com.devsuperior.dscatalog.dto.UserInsertDTO;
 import com.devsuperior.dscatalog.entities.Role;
 import com.devsuperior.dscatalog.entities.User;
 import com.devsuperior.dscatalog.repositories.RoleRepository;
@@ -24,6 +26,9 @@ import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 @Service
 public class UserService {
 
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
 	@Autowired
 	private UserRepository repository;
 	
@@ -49,10 +54,11 @@ public class UserService {
 	}
 
 	@Transactional
-	public UserDTO insert(UserDTO dto) {
+	public UserDTO insert(UserInsertDTO dto) {
 
 		User entity = new User();
 		copyDtoToEntity(dto, entity);
+		entity.setPassword(passwordEncoder.encode(dto.getPassword()));
 		entity = repository.save(entity);
 
 		return new UserDTO(entity);
@@ -90,9 +96,9 @@ public class UserService {
 	
 	private void copyDtoToEntity(UserDTO dto, User entity) {
 		
-		entity.setFirstName(entity.getFirstName());
-		entity.setLastName(entity.getLastName());
-		entity.setEmail(entity.getEmail());
+		entity.setFirstName(dto.getFirstName());
+		entity.setLastName(dto.getLastName());
+		entity.setEmail(dto.getEmail());
 		
 		entity.getRoles().clear();
 		for(RoleDTO roleDto : dto.getRoles()) {
